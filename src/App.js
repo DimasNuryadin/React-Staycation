5ef28b11a5b6cb41c244f67e774ec288feeabc4b
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2024-12-15 19:49:13 +0700

    setup react and create button component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "assets/scss/style.scss"
+import Button from "elements/Button";
+
+function App() {
+  return (
+    <div className="App">
+      Hello world!
+      <Button />
+    </div>
+  );
+}
+
+export default App;
