c7f1987bfa7f0a7178d21849f0147f590ff73975
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2024-12-17 08:13:03 +0700

    create hero component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,13 @@
 import Header from "parts/Header"
 import React from 'react'
+import landingPage from 'json/landingPage.json'
+import Hero from "parts/Hero"
 
 export default function LandingPage() {
   return (
     <div>
       <Header></Header>
+      <Hero data={landingPage.hero} />
     </div>
   )
 }
