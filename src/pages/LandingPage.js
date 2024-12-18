027584e67c0b3405d29493d5637bf20d072ed5a5
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2024-12-18 15:23:10 +0700

    create footer component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,6 +5,7 @@ import MostPicked from "parts/MostPicked"
 import React, { Component } from 'react'
 import Categories from "parts/Categories"
 import Testimony from "parts/Testimony"
+import Footer from "parts/Footer"
 
 export default class LandingPage extends Component {
   // Akan dijalankan pertama
@@ -20,6 +21,7 @@ export default class LandingPage extends Component {
         <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
         <Categories data={landingPage.categories} />
         <Testimony data={landingPage.testimonial} />
+        <Footer />
       </>
     )
   }
